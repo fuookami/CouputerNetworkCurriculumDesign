aa7c13fbdec84eed4b7a4a3ba7a6d303c5c7f2a5
fuookami <bagawolf2014@yahoo.co.jp>
2017-04-03 22:50:41 +0800

    重组项目

--- CNSD1/Server/main.cpp
+++ CNSD1/Server/main.cpp
@@ -1,15 +1,11 @@
 #include <QtWidgets/QApplication>
 #include "Server.h"
-#include "Cilent.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
 	Server server;
-	Cilent cilent;
-
-	cilent.connectToHost(BasicSetting::host, BasicSetting::port);
 
 	return a.exec();
 }
