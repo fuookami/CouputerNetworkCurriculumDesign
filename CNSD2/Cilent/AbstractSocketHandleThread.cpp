f9dde8db0a8dc20899923763ea2734ba7c288098
fuookami <bagawolf2014@yahoo.co.jp>
2017-04-08 04:24:56 +0800

    重建Socket收发管程框架

--- CNSD2/Cilent/AbstractSocketHandleThread.cpp
+++ CNSD2/Cilent/AbstractSocketHandleThread.cpp
@@ -6,9 +6,101 @@ void SocketHandleThread::run()
 
 	while (!stopped)
 	{
-		if (threadState == Public::Idle || threadState == Public::Receieving)
+		if (threadState == Public::ThreadState::Idle)
+		{
+			if (sendingInfo.sendingData.empty())
+				tcpSocket->waitForReadyRead(Public::MSOfTimePart);
+			else
+			{
+				// 转至准备发送态并发送SYN信号
+				threadState = Public::ThreadState::WaitForSending;
+				sendFrame(Public::RequestTypes::SYN, Public::countFrames(sendingInfo.sendingData.front()));
+			}
+		}
+		else if (threadState == Public::ThreadState::Receieving
+			|| threadState == Public::ThreadState::WaitForSending)
 		{
 			tcpSocket->waitForReadyRead(Public::MSOfTimePart);
 		}
+		else if (threadState == Public::ThreadState::Sending)
+		{
+			// 查看是否有可发送的数据包
+			// 如果有，发送
+			// 没有则等待ACK回应
+			if (checkDataDeque())
+				sendFrames();
+			else	
+				tcpSocket->waitForReadyRead(Public::MSOfTimePart);
+		}
 	}
+}
+
+void SocketHandleThread::dataReceived()
+{
+	Public::State currFrameState(Public::getRandomFrameState());
+	QDataStream in(tcpSocket);
+	Public::DataFrame currFrame(in);
+
+	std::ostringstream sout;
+	sout << "当前随机得到的帧状态为" << Public::getFrameStateString(currFrameState) << std::endl;
+	emit pushMsg(QString::fromLocal8Bit(sout.str().c_str()));
+
+	if (currFrameState == Public::FrameState::Lose)
+		return;
+	if (currFrameState == Public::FrameState::Wrong
+		|| !currFrame.isCorrect())
+	{
+		emit pushMsg(QString::fromLocal8Bit("帧校验出错，丢弃该帧\n"));
+		return;
+	}
+
+	sout.clear();
+	sout << "帧校验正确，数据为：" << currFrame.data << std::endl;
+	emit pushMsg(QString::fromLocal8Bit(sout.str().c_str()));
+	emit pushMsg(QString::fromLocal8Bit("准备进行解密\n"));
+	Public::decode(currFrame.data);
+	emit pushMsg(QString::fromLocal8Bit("解密完成\n"));
+	sout.clear();
+	sout << "解密后数据为：" << currFrame.data << std::endl;
+	emit pushMsg(QString::fromLocal8Bit(sout.str().c_str()));
+
+	switch (threadState)
+	{
+	case Public::ThreadState::Idle:
+		dataReceivedForIdea(currFrame, currFrameState);
+		break;
+	case Public::ThreadState::Receieving:
+		dataReceivedForReceiving(currFrame, currFrameState);
+		break;
+	case Public::ThreadState::Sending:
+		dataReceivedForSending(currFrame, currFrameState);
+		break;
+	case Public::ThreadState::WaitForSending:
+		dataReceivedForWaitSending(currFrame, currFrameState);
+		break;
+	default:
+		break;
+	}
+}
+
+void SocketHandleThread::dataReceivedForIdea(const Public::DataFrame &currFrame, Public::State frameState)
+{
+	// 如果收到SYN转移到收状态，并返回ACK(-1)
+}
+
+void SocketHandleThread::dataReceivedForReceiving(const Public::DataFrame &currFrame, Public::State frameState)
+{
+	// 帧轮盘接收数据，并返回对应的ACK
+	// 如果数据接收完毕，向上级发送数据并转移到空闲状态，并返回ACK(-1)示意对方发送完毕
+}
+
+void SocketHandleThread::dataReceivedForWaitSending(const Public::DataFrame &currFrame, Public::State frameState)
+{
+	// 如果收到ACK(-1)转移到发送状态
+}
+
+void SocketHandleThread::dataReceivedForSending(const Public::DataFrame &currFrame, Public::State frameState)
+{
+	// 如果发送完毕或收到ACK(-1)，则转移到空闲状态
+	// 否则，移动发送窗口
 }
--- CNSD2/Cilent/AbstractSocketHandleThread.h
+++ CNSD2/Cilent/AbstractSocketHandleThread.h
@@ -2,6 +2,7 @@
 
 #include "Public.h"
 #include <set>
+#include <QtCore/QTimer>
 #include <QtCore/QThread>
 #include <QtNetwork/QTcpSocket>
 
@@ -19,24 +20,63 @@ struct SendingInfo
 	unsigned int waitingAcceptId;
 	unsigned int lastSendedId;
 	Public::DataDeque sendingData;
+	std::array<PKTTimer *, Public::RouletteSize> timers;
+};
+
+class PKTTimer : public QObject
+{
+	Q_OBJECT;
+
+public:
+	PKTTimer(unsigned char _id);
+	void startTimer(const unsigned short ms = Public::MSOfOnceTry);
+	void stopTimer();
+
+signals:
+	void timeoutSignal(unsigned char);
+
+	private slots:
+	void timeoutSlot(void);
+
+private:
+	QTimer *timer;
+	unsigned char id;
 };
 
 class SocketHandleThread : public QThread
 {
+	Q_OBJECT;
+
 public:
 	SocketHandleThread(QTcpSocket *_tcpSocket, unsigned int id);
+	void start();
+	template<class T>
+	void sendData(const T &data);
 
 signals:
-	void pushMsgSignal(const QString msg);
+	void pushMsg(const QString msg);
 	void pushData(const std::string data);
 
 protected:
 	void run();
 
 private slots:
-	void dataReceived();
+	void dataReceived(void);
+	void PKTTimeOut(unsigned char id);
 
 private:
+	bool checkDataDeque(void);
+	void sendFrames(void);
+	void sendFrame(Public::RequestType requestType, unsigned int data);
+
+	void dataReceivedForIdea(const Public::DataFrame &currFrame, Public::State frameState = Public::FrameState::FrameNoError);
+	void dataReceivedForReceiving(const Public::DataFrame &currFrame, Public::State frameState = Public::FrameState::FrameNoError);
+	void dataReceivedForWaitSending(const Public::DataFrame &currFrame, Public::State frameState = Public::FrameState::FrameNoError);
+	void dataReceivedForSending(const Public::DataFrame &currFrame, Public::State frameState = Public::FrameState::FrameNoError);
+
+private:
+	static unsigned int threadCounter;
+
 	RecievingInfo recievingInfo;
 	SendingInfo sendingInfo;
 
